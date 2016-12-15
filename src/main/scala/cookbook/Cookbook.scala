a4a3a6a543305bd32e369c7c68061eaa07f5143e
Craig Duncan <craig@CraigMBPR.local>
2016-12-15 14:55:45 +0000

    3.5 Apply polymorphism and inheritance

--- src/main/scala/cookbook/Cookbook.scala
+++ src/main/scala/cookbook/Cookbook.scala
@@ -1,16 +1,48 @@
 package cookbook
 
-// Using a case class gives apply/unapply, equals, hashcode, copy and toString for free
+// Using a case class provides apply/unapply, equals, hashcode, copy and toString for free
 // called case classes because we use them in pattern matching
-case class Recipe (ingredients: List[String], directions: List[String])
+case class Recipe (ingredients: Map[String, Mass], directions: List[String]) {
+  def shoppingList(kitchen: Map[String, Mass]): List[String] =
+    for {
+      (name, need) <- ingredients.toList
+      have = kitchen.getOrElse(name, Grams(0))
+      if have.compareTo(need) < 0
+    } yield name
+}
+
+// This is an algebraic data type
+// sealed keyword means that no other classes outside this file can extend this
+// this can be useful to ensure that pattern matching can be exhaustive
+// general rule, make abstract defs so you can override
+sealed abstract class Mass extends Comparable[Mass]{
+  def amount: Double
+  def toGrams : Grams
+  def compareTo(that: Mass): Int = (this.toGrams.amount - that.toGrams.amount).toInt
+}
+case class Grams(amount: Double) extends Mass {
+  override def toGrams: Grams = this
+  override def toString: String = amount + "g"
+}
+case class Milligrams(amount: Double) extends Mass {
+  override def toGrams: Grams = Grams (amount / 1000)
+  override def toString: String = amount + "mg"
+}
+case class Kilograms(amount: Double) extends Mass {
+  override def toGrams: Grams = Grams (amount * 1000)
+  override def toString: String = amount + "kg"
+}
+
 
 object Cookbook {
   val pbj = Recipe(
-    List("peanut butter", "jelly", "bread"),
+    Map("peanut butter" -> new Grams(10),
+      "jelly" -> new Kilograms(0.5),
+      "bread" -> new Grams(250)),
     List("put the peanut butter and jelly on the bread"))
 
   val baconPancakes = Recipe(
-    List("bacon", "pancakes"),
+    Map("bacon" -> new Kilograms(1), "pancakes" -> new Milligrams(10)),
     List("take some bacon and put it in a pancake"))
 
 }
--- src/test/scala/cookbook/CookbookSpec.scala
+++ src/test/scala/cookbook/CookbookSpec.scala
@@ -5,15 +5,15 @@ import org.scalatest.{FlatSpec, Matchers}
 class CookbookSpec extends FlatSpec with Matchers{
 
   "Recipes r1 and r2" should " be the same" in {
-    val r1 = Recipe(List.empty, List.empty)
-    val r2 = Recipe(List.empty, List.empty)
+    val r1 = Recipe(Map.empty, List.empty)
+    val r2 = Recipe(Map.empty, List.empty)
     assert (r1 == r2)
   }
 
   "Using a case class" should "allow us to see use the unapply method to extract information" in {
     Cookbook.pbj match {
       case Recipe(ingredients, directions) =>
-        assert(ingredients == List("peanut butter", "jelly", "bread"))
+        assert(ingredients == Map("peanut butter" -> Grams(10), "jelly" -> Kilograms(0.5), "bread" -> Grams(250)))
         assert(directions == List("put the peanut butter and jelly on the bread"))
     }
   }
