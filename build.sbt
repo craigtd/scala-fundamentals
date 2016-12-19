d1f0509150369dd7177bce8d2fe5fce426b0ad2b
Craig Duncan <craig@CraigMBPR.local>
2016-12-19 11:59:19 +0000

    Introduced ScalaCheck so can use table features of PropertyCheck

--- build.sbt
+++ build.sbt
@@ -6,8 +6,10 @@ scalaVersion := "2.11.8"
 
 libraryDependencies ++= Seq(
   "org.scalatest" %% "scalatest" % "3.0.1" % "test",
+  "org.scalacheck" %% "scalacheck" % "1.13.4" % "test",
   "org.pegdown"    %  "pegdown"     % "1.6.0"  % "test"
 )
+
 
 testOptions in Test ++= Seq(
   Tests.Argument(TestFrameworks.ScalaTest, "-o"),
--- src/test/scala/calculator/CalculatorSpec.scala
+++ src/test/scala/calculator/CalculatorSpec.scala
@@ -1,27 +1,44 @@
 package calculator
 
 import calculator.Calculator._
+import org.scalatest.prop.PropertyChecks
 import org.scalatest.{FlatSpec, Matchers}
 
-class CalculatorSpec extends FlatSpec with Matchers{
+class CalculatorSpec extends FlatSpec with Matchers with PropertyChecks{
 
   "1 1 +" should "return 2" in {
     val expression = (parse("1 1 +"))
-    assert(calculate(expression) == 2)
+    calculate(expression) shouldBe 2
     println(s"${toInfix(expression)}")
   }
 
   "1 2 3 + -" should "return -4" in {
     val expression = (parse("1 2 3 + -"))
-    assert(calculate(expression) == -4)
+    calculate(expression) shouldBe -4
     println(s"${toInfix(expression)}")
   }
 
   "1 2 + 3 4 5 - - * 6 7 8 - * /" should "return -2" in {
     val expression = parse("1 2 + 3 4 5 - - * 6 7 8 - * /")
-    assert(calculate(expression) == -2)
+    calculate(expression) shouldBe -2
+
     println(s"${toInfix(expression)}")
   }
+
+
+  "postfix expressions" should "convert to the infix version" in {
+    val input = Table(
+      "postfix expression"            -> "infix expression",
+      //==================================================
+      "1 1 +"                         -> "(1 + 1)",
+      "1 2 3 + -"                     -> "(1 - (2 + 3))",
+      "1 2 + 3 4 5 - - * 6 7 8 - * /" -> "(((1 + 2) * (3 - (4 - 5))) / (6 * (7 - 8)))"
+    )
+    forAll(input) {
+      case (postfix, infix) => toInfix(parse(postfix)) shouldBe infix
+    }
+  }
+
 
 
   it should "produce IllegalArgumentException when garbage is provided" in {
